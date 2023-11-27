c350f947cfbdbe4ffef46974996ed499d2d00b6a
Simone <simonestormbk@gmail.com>
2023-11-27 09:09:34 +0100

    Domain oprettet

---
+++ Shared/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public ICollection<Book> Books { get; set; }
+}
