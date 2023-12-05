dccd1c7f81d3a7b736e17c47e759f31a6b798c1e
Simone <simonestormbk@gmail.com>
2023-12-05 12:12:23 +0100

    RegisterDto.cs

---
+++ Shared/DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.DTOs;
+
+public class RegisterDto
+{
+    public string Username { get; }
+    public string Password { get; }
+
+    public RegisterDto(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
