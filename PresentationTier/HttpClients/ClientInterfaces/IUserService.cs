c30fb8e4d7c0f73af4528161a3e61f78b8ac821f
Simone <simonestormbk@gmail.com>
2023-12-07 09:05:17 +0100

    Httpclient service

--- PresentationTier/HttpClients/ClientInterfaces/IUserService.cs
+++ PresentationTier/HttpClients/ClientInterfaces/IUserService.cs
@@ -1,6 +1,12 @@
-﻿namespace HttpClients.ClientInterfaces;
+﻿using Shared.DTOs;
+
+namespace HttpClients.ClientInterfaces;
 
 public interface IUserService
 {
-    
+    Task RegisterUserAsync(string username, string password, string phoneNumber, string email);
+
+    Task<string> LoginUser(string username, string password);
+
+
 }
--- PresentationTier/HttpClients/Implementations/UserHttpClient.cs
+++ PresentationTier/HttpClients/Implementations/UserHttpClient.cs
@@ -4,5 +4,13 @@ namespace HttpClients.Implementations;
 
 public class UserHttpClient:IUserService
 {
-    
+    public Task RegisterUserAsync(string username, string password, string phoneNumber, string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> LoginUser(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
 }
